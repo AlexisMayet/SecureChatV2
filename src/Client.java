1874352aba67d8ade0dbf0b5b7a603851e38caf6
youssefbm12 <72268364+youssefbm12@users.noreply.github.com>
2022-09-22 14:59:31 +0200

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import com.google.gson.JsonArray;
@@ -13,8 +14,8 @@ import com.google.gson.JsonParser;
 public class Client implements Serializable {
 
 	/**
-	* 
-	*/
+	 *
+	 */
 	private static final long serialVersionUID = 1L;
 	private String username;
 	private String password;
@@ -120,6 +121,7 @@ public class Client implements Serializable {
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
 			objectOutputStream.writeObject(client);
 
+			counter_method();
 			// client.listenForMessage();
 			// client.sendMessage();
 
@@ -130,36 +132,83 @@ public class Client implements Serializable {
 		} catch (JsonParseException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public String getID() {
 
 		return username;
 	}
+	public static void counter_method(){
+		Scanner value = new Scanner(System.in);
+		while (true) {
+			boolean name = test();
+			if(name) {
+				System.out.println("How much do you want to increase ");
+				double value_1 = value.nextDouble();
+				Server.counter += value_1;
+				System.out.println("The counter:" + Server.counter);
+			}
+			else {
+				System.out.println("How much do you want to decrease ");
+				double value_1 = value.nextDouble();
+				Server.counter = Server.counter - value_1;
+				System.out.println("The counter is equal to : " + Server.counter);
+			}
 
+			if(test2())
+				return;
+		}
+	}
+	public static  boolean test(){
+		while(true) {
+			Scanner increase_or_decrease = new Scanner(System.in);
+			System.out.println("The counter is equal to :" + Server.counter);
+			System.out.println("Do you want to : increase or decrease");
+			String userName = increase_or_decrease.nextLine();
+			if ("increase".equalsIgnoreCase(userName))
+				return true;
+			else if ("decrease".equalsIgnoreCase(userName))
+				return false;
+			else
+				System.out.println("Try again");
+		}
+	}
+	public static boolean test2(){
+		while(true) {
+			Scanner increase_or_decrease = new Scanner(System.in);
+			System.out.println("Do you want to finish ? no/yes");
+			String userName_2 = increase_or_decrease.nextLine();
+			if ("yes".equalsIgnoreCase(userName_2))
+				return true;
+			else if ("no".equalsIgnoreCase(userName_2))
+				return false;
+			else
+				System.out.println("Try again");
+		}
+
+	}
 }
 /*
- * 
+ *
  * public static void main(String[] args) throws IOException { // need host and
  * port, we want to connect to the ServerSocket at port 7777 Socket socket = new
  * Socket("localhost", 7777); System.out.println("Connected!");
- * 
+ *
  * // get the output stream from the socket. OutputStream outputStream =
  * socket.getOutputStream(); // create an object output stream from the output
  * stream so we can send an // object through it ObjectOutputStream
  * objectOutputStream = new ObjectOutputStream(outputStream);
- * 
+ *
  * // make a bunch of messages to send. List<Message> messages = new
  * ArrayList<>(); messages.add(new Message("Hello from the other side!"));
  * messages.add(new Message("How are you doing?")); messages.add(new
  * Message("What time is it?")); messages.add(new Message("Hi hi hi hi."));
- * 
+ *
  * System.out.println("Sending messages to the ServerSocket");
  * objectOutputStream.writeObject(messages);
- * 
+ *
  * System.out.println("Closing socket and terminating program.");
  * socket.close(); }
- * 
+ *
  * }
  */
